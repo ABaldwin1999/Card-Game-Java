96a8f4293fa8c9175027ed3f260b02385f29e5d7
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-05 18:51:17 +0100

    player class

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,9 +1,42 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Scanner;
 
 public class Player {
-    private boolean playerTurn;
     private String playerName;
-    private ArrayList<Card> playerDeck = new ArrayList<>();
+    //private ArrayList<Card> playerDeck = new ArrayList<>();
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+    public static String inputName(int num){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input player name");
+        String name = "";
+        try {
+            name =scanner.nextLine();
+        }
+        catch(Exception e) {
+            inputName(num);
+        }
+        if (name ==""){
+            inputName(num);
+        }
+        System.out.println("Player " + num + ": " + scanner.next());
+        return name;
+    }
+    public static Player chooseCurrentPlayer(Player player1, Player player2, int i){
+        if((i+1)%2 ==0){
+            return player1 ;
+        }
+        else{
+            return player2;
+        }
+    }
 }
--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -12,24 +12,26 @@ public class Snap extends CardGame {
         }
     }
 
-    public void winGame(){
-        if (snap == true) {
-            System.out.println("You won");
+    public void winGame(Player currentPlayer){
+        if (snap) {
+            System.out.println("You won"+currentPlayer.getPlayerName());
         }
     }
 
     public void playSnap() {
+        Player player1 = new Player(Player.inputName(1));
+        Player player2 = new Player(Player.inputName(2));
         System.out.println("Press enter to play");
         for (int i = 0; i < 51; i++) {
+            Player currentPlayer = Player.chooseCurrentPlayer(player1,player2,i);
             snapRound(i);
             Scanner scanner = new Scanner(System.in);
             String declare = "";
             try {
                 declare = scanner.next();
-            } catch (Exception e) {
-            }
-            if(declare.toLowerCase()=="snap"){
-                winGame();
+            } catch (Exception e) {}
+            if(declare.equalsIgnoreCase("snap")){
+                winGame(currentPlayer);
             }
         }
 
